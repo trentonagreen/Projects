c116018672387b89ccaba34df153ac056c24e6b6
Trenton Green <trentonagreen@gmail.com>
2019-05-18 12:25:57 -0700

    Initial commit of mecha game unity project

---
+++ Unity/Mecha/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform transformDrone;
+    void Awake()
+    {
+        transformDrone = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public Vector3 velocityCameraFollow;
+    public Vector3 behindPosition = new Vector3(0, 4, -7);
+    public float angle = 15f;
+    void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, transformDrone.transform.TransformPoint(behindPosition) + Vector3.up * Input.GetAxis("Vertical"), ref velocityCameraFollow, 0.1f);
+        //transform.rotation = Quaternion.Euler(new Vector3(angle, transformDrone.GetComponent<FlyMovement>().currRotationY, 0));
+        transform.rotation = Quaternion.Euler(new Vector3(angle, transformDrone.GetComponent<FlyMovement>().currRotationY, 0));
+    }
+}
---
+++ Unity/Mecha/Assets/Scripts/FlyMovement.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMovement : MonoBehaviour
+{
+    Rigidbody rbDrone;
+
+    void Awake()
+    {
+        rbDrone = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.K))
+        {
+            rbDrone.constraints = RigidbodyConstraints.None;
+        }
+        if (Input.GetKeyUp(KeyCode.I) || Input.GetKeyUp(KeyCode.K))
+        {
+            rbDrone.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+        //rbDrone.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+
+    void FixedUpdate()
+    {
+        FlyUpDown();
+        FlyForward();
+        FlyRotation();
+        FlyClampValues();
+        FlyLR();
+
+        rbDrone.AddRelativeForce(Vector3.up * upForce);
+        //rbDrone.rotation = Quaternion.Euler(new Vector3(tiltAmountForward, currRotationY, tiltAmountSide));
+        rbDrone.rotation = Quaternion.Euler(new Vector3(rbDrone.rotation.x, currRotationY, rbDrone.rotation.z));
+    }
+
+    public float upForce;
+    void FlyUpDown() 
+    {
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            if (Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.K))
+            {
+                rbDrone.velocity = rbDrone.velocity;
+            }
+            if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && !Input.GetKey(KeyCode.J) && !Input.GetKey(KeyCode.L))
+            {
+                upForce = 450;
+                rbDrone.velocity = new Vector3(rbDrone.velocity.x, Mathf.Lerp(rbDrone.velocity.y, 0, Time.deltaTime * 5), rbDrone.velocity.z);
+                //upForce = 281;
+            }
+            if (!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && !Input.GetKey(KeyCode.J) || !Input.GetKey(KeyCode.L))
+            {
+                upForce = 450;
+                rbDrone.velocity = new Vector3(rbDrone.velocity.x, Mathf.Lerp(rbDrone.velocity.y, 0, Time.deltaTime * 5), rbDrone.velocity.z);
+                //upForce = 110;
+            }
+            if (Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.L))
+            {
+                upForce = 410;
+            }
+        }
+        if(Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            //upForce = 135;
+            upForce = 400;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            upForce = 450;
+            if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+            {
+                upForce = 500;
+            }
+        }
+        else if(Input.GetKey(KeyCode.K))
+        {
+            upForce = -200;
+        }
+        else if(!Input.GetKey(KeyCode.I) && !Input.GetKey(KeyCode.K) && (Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f))
+        {
+            upForce = 98.1f;
+        }
+
+    }
+
+    public float moveForwardSpeed = 500;
+    public float tiltAmountForward = 0f;
+    public float tiltVelocityForward;
+    void FlyForward()
+    {    
+        if (System.Math.Abs(Input.GetAxis("Vertical") - Mathf.Epsilon) > 0f)
+        {
+            rbDrone.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * moveForwardSpeed);
+            tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, 20 * Input.GetAxis("Vertical"), ref tiltVelocityForward, 0.1f);
+        }
+    }
+
+    public float wantRotationY;
+    public float currRotationY;
+    public float rotateAmountByKeys = 2.5f;
+    public float rotationVelocityY;
+    void FlyRotation()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            wantRotationY -= rotateAmountByKeys;
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            wantRotationY += rotateAmountByKeys;
+        }
+
+        currRotationY = Mathf.SmoothDamp(currRotationY, wantRotationY, ref rotationVelocityY, 0.25f);
+    }
+
+    public Vector3 smoothDampVelocity;
+    void FlyClampValues()
+    {
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            rbDrone.velocity = Vector3.ClampMagnitude(rbDrone.velocity, Mathf.Lerp(rbDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
+        }
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            rbDrone.velocity = Vector3.ClampMagnitude(rbDrone.velocity, Mathf.Lerp(rbDrone.velocity.magnitude, 10.0f, Time.deltaTime * 5f));
+        }
+        if(Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            rbDrone.velocity = Vector3.ClampMagnitude(rbDrone.velocity, Mathf.Lerp(rbDrone.velocity.magnitude, 5.0f, Time.deltaTime * 5f));
+        }
+        if(Mathf.Abs(Input.GetAxis("Vertical")) < 0.2f && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.2f)
+        {
+            rbDrone.velocity = Vector3.SmoothDamp(rbDrone.velocity, Vector3.zero, ref smoothDampVelocity, 0.95f);
+        }
+    }
+
+    public float sideMovementSpeed = 300f;
+    public float tiltAmountSide;
+    public float tiltVelocitySide;
+    public void FlyLR()
+    {
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2f)
+        {
+            rbDrone.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * sideMovementSpeed);
+            tiltAmountSide = Mathf.SmoothDamp(tiltAmountSide, -20 * Input.GetAxis("Horizontal"), ref tiltVelocitySide, 0.1f);
+        }
+        else
+        {
+            tiltAmountSide = Mathf.SmoothDamp(tiltAmountSide, 0, ref tiltVelocitySide, 0.1f);
+        }
+    }
+}

b50d07ce8ae323c35e7d781627c30a7421ea28c8
Trenton Green <trentonagreen@gmail.com>
2019-07-09 22:19:53 -0700

    Brand new rigidbody character controller

---
+++ Unity/Mecha/Assets/Scripts/3rdPerson/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /* TODO
+     *      - Add Strafing
+     *      - Add Blink / Dash / Dodge
+     *      - Add Animations
+     *          - Idle
+     *          - Walk
+     *          - Run
+     * 
+     */
+
+    [Header("Speed Settings")]
+    public float walk_speed;
+    public float run_speed;
+    private float speed;
+
+    [Header("Hover Settings")]
+    public float hover_force;
+    public ForceMode force_type;
+
+    [Header("Rotate Settings")]
+    public float turn_speed;
+    private Vector3 dir_pos;
+    private Vector3 stor_dir;
+
+    Rigidbody rb;
+    Transform cam;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        #region Inputs
+
+        // Sprint
+        if (Input.GetButton("PS4_Circle") || Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = run_speed;
+        }
+        else if (speed != walk_speed)
+        {
+            speed = walk_speed;
+        }
+
+        // Hover
+        if((Input.GetButton("PS4_L1")))
+        {
+            rb.useGravity = false;
+            Hover(hover_force, force_type);
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
+        #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        #region Movement
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(hor, 0, ver);
+        direction = direction.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + direction);
+        #endregion
+
+        #region Rotation
+        stor_dir = cam.right;
+        dir_pos = transform.position + (stor_dir * hor) + (cam.forward * ver);
+
+        Vector3 dir = dir_pos - transform.position;
+        dir.y = 0;
+
+        if(hor != 0 || ver != 0)
+        {
+            float angle = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(dir));
+
+            if(angle != 0)
+            {
+                rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), turn_speed * Time.deltaTime);
+            }
+        }
+
+        Debug.DrawRay(transform.position, dir, Color.red);
+        #endregion
+    }
+
+
+    void Hover(float hover_force, ForceMode type)
+    {
+        rb.AddRelativeForce(transform.up * hover_force, type);
+    }
+
+}

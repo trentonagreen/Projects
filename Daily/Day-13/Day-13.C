d1667cf2dde874173295fc66a6ff9187240666c0
Trenton Green <trentonagreen@gmail.com>
2019-09-09 11:29:25 -0700

    Cleaning up school projects file

--- Daily/Day-13/Day-13.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv) {
-  printf("Daily Programming, Day-13\n");
-  printf("-----------------------------------------------------------------\n");
-
-}
